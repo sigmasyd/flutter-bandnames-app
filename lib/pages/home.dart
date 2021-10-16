4ee47de628d0399d668773032284961c6f13ee52
Carlos Barcos <fbarcos8@hotmail.com>
2021-10-16 03:33:40 -0500

    Agregando logica para interaccion de borrado.. no se borra .. solo se agrega interaccion de dismiss
    	modificado:     lib/pages/home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -36,17 +36,29 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  ListTile _bandTile(Band band) {
-    return ListTile(
-      leading: CircleAvatar(
-        child: Text(band.name.substring(0, 2)),
-        backgroundColor: Colors.blue[100],
+  Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      background: Container(
+        padding: EdgeInsets.only(left: 8.0),
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text('Delete Band', style: TextStyle(color: Colors.white))
+        ),
       ),
-      title: Text(band.name),
-      trailing: Text('${band.votes}', style: TextStyle(fontSize: 20)),
-      onTap: () {
-        print(band.name);
-      },
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(band.name.substring(0, 2)),
+          backgroundColor: Colors.blue[100],
+        ),
+        title: Text(band.name),
+        trailing: Text('${band.votes}', style: TextStyle(fontSize: 20)),
+        onTap: () {
+          print(band.name);
+        },
+      ),
     );
   }
 
