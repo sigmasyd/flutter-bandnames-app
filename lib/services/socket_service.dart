2ffcf16049792bcf8d0dd6a3783cd0aa30d84de3
Carlos Barcos <fbarcos8@hotmail.com>
2021-11-24 01:56:00 -0500

    Se agrega/configura cliente socket.io para flutter Se elimina directorio de pruebas.
    	modificado:     lib/main.dart
    	nuevo archivo:  lib/pages/status.dart
    	nuevo archivo:  lib/services/socket_service.dart
    	modificado:     pubspec.lock
    	modificado:     pubspec.yaml
    	borrado:        test/widget_test.dart

---
+++ lib/services/socket_service.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus { Online, Offline, Connecting }
+
+// ayudara a decirle a provider cuando refrescar la interface o redibujar
+class SocketService with ChangeNotifier {
+  IO.Socket _socket = IO.io("http://192.168.100.12:3000/");
+
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+
+  IO.Socket get socket => _socket;
+  Function get emit => _socket.emit;
+
+  SocketService() {
+    //this._initConfig();
+    _initConfig();
+  }
+
+  void _initConfig() {
+    _socket = IO.io("http://192.168.100.12:3000/", {
+      'transports': ['websocket'],
+      'autoConnect': true,
+    });
+
+    _socket.on('connect', (_) {
+      print('Flutter Connect...');
+    });
+
+    _socket.on('disconnect', (_) => print('Flutter Disconnect'));
+  }
+}
